2a2f1c2f02187ad61254d79fe3603be130a635eb
Ubuntu <ubuntu@ip-172-31-20-57.us-east-2.compute.internal>
2021-09-29 18:09:50 +0000

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
     def self.all_ratings
-        self.all.select(:rating).distinct.pluck(:rating)
+        # self.all.select(:rating).distinct.pluck(:rating)
+        Movie.select(:rating).distinct.inject([]) { |a, m| a.push m.rating }
     end
     
     def self.with_ratings(list)
