19bce6bf1fa027d84399358f776b670e5c9b06dd
Ubuntu <ubuntu@ip-172-31-20-57.us-east-2.compute.internal>
2021-09-30 05:40:21 +0000

    update style

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,17 +1,17 @@
 class Movie < ActiveRecord::Base
-    def self.all_ratings
-        # Movie.all.select(:rating).distinct.pluck(:rating)
-        ['G','PG','PG-13','R']
-        # Movie.select(:rating).distinct.inject([]) { |a, m| a.push m.rating }
+  def self.all_ratings
+    # Movie.all.select(:rating).distinct.pluck(:rating)
+    ['G','PG','PG-13','R']
+    # Movie.select(:rating).distinct.inject([]) { |a, m| a.push m.rating }
+  end
+  
+  def self.with_ratings(list)
+    if list.empty?
+        Movie.all
+    else
+        list = list.map {|rating| rating.upcase}
+        Movie.where(rating: list)
     end
-    
-    def self.with_ratings(list)
-        if list.empty?
-            Movie.all
-        else
-            list = list.map {|rating| rating.upcase}
-            Movie.where(rating: list)
-        end
-        
-    end
+      
+  end
 end
