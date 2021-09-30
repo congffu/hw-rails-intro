9aa718ece2b2e24df5627e01e82363077d91a924
Ubuntu <ubuntu@ip-172-31-20-57.us-east-2.compute.internal>
2021-09-30 04:43:17 +0000

    part 3 test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
     def self.all_ratings
-        # Movie.all.select(:rating).distinct.pluck(:rating)
-        ['G','PG','PG-13','R']
+        Movie.all.select(:rating).distinct.pluck(:rating)
+        # ['G','PG','PG-13','R']
         # Movie.select(:rating).distinct.inject([]) { |a, m| a.push m.rating }
     end
     
