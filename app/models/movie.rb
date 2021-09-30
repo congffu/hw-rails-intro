c840cc2591a1917331047d711b9e4c11b2841c6b
Ubuntu <ubuntu@ip-172-31-20-57.us-east-2.compute.internal>
2021-09-30 15:10:18 +0000

    part 3 partial working

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
     def self.all_ratings
-        # Movie.all.select(:rating).distinct.pluck(:rating)
-        ['G','PG','PG-13','R']
+        Movie.all.select(:rating).distinct.pluck(:rating)
+        # ['G','PG','PG-13','R']
     end
     
     def self.with_ratings(list)
