a5892a03b96ff653ebe0fb91ee20f402db07e01b
Ubuntu <ubuntu@ip-172-31-20-57.us-east-2.compute.internal>
2021-09-30 05:49:05 +0000

    revert to part 3 partial working version

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,24 +14,51 @@ class MoviesController < ApplicationController
       if params[:ratings]
         @rating_list = params[:ratings].keys
       else
-        @rating_list = @all_ratings
+        if session[:ratings]
+          @rating_list = session[:ratings]
+        else
+          @rating_list = @all_ratings
+        end
+      end
+      
+      if @rating_list != session[:ratings]
+        session[:ratings] = @rating_list
       end
       
       @movies = Movie.with_ratings(@rating_list)
       @rating_hash = Hash[@rating_list.map {|rating| [rating, '1']}]
       
       
-      
       if params[:sort_by]
+        @sort = params[:sort_by]
+      else
+        if session[:sort_by]
+          @sort = session[:sort_by]
+        else
+          @sort = ''
+        end
+      end
         
-        @movies = @movies.order(params[:sort_by])
-        if params[:sort_by] == 'title'
+      if @sort != session[:sort_by]
+        session[:sort_by] = @sort
+      end
+        
+      flash.keep
+      if @sort
+        @movies = @movies.order(@sort)
+        if @sort == 'title'
           @title_header = 'hilite bg-warning'
-        elsif params[:sort_by] == 'release_date'
+        elsif @sort == 'release_date'
           @release_date_header = 'hilite bg-warning'
         end
-  
       end
+      
+      # @movies = @movies.order(params[:sort_by])
+      # if params[:sort_by] == 'title'
+      #   @title_header = 'hilite bg-warning'
+      # elsif params[:sort_by] == 'release_date'
+      #   @release_date_header = 'hilite bg-warning'
+      # end
           
     end
   
